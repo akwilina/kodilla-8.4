8903a88370aab0f3fad2698590de2108bbe5b1b0
delola\delola lokal <aleksandra.man@gmail.com>
2017-02-28 22:06:23 +0100

    added spaces between arguments and operators

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 var a = prompt("podaj wartość a (domyślna wartość to 1)", 1);
 var h = prompt("podaj wartość h (domyślna wartość to 2)", 2);
-var triangleArea = a*h/2;
+var triangleArea = a * h / 2;
 
 console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
 alert('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
